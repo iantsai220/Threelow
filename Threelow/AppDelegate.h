434631788176f1bb3ce4dd01c0c132356b2af543
iantsai <iantsai86@gmail.com>
2015-05-12 18:41:57 -0700

    Initial Commit

---
+++ Threelow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Threelow
+//
+//  Created by Ian Tsai on 2015-05-12.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
